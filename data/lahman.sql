13f6325da284262c2bc46dad866105e26e61b072
JSMitchem <sethmitchem@outlook.com>
2022-06-08 15:55:44 -0500

    Loading..

--- data/lahman.sql
+++ data/lahman.sql
@@ -1,32 +1,3 @@
-SELECT * FROM allstarfull;
-SELECT * FROM appearances;
-SELECT * FROM awardsmanagers;
-SELECT * FROM awardsplayers;
-SELECT * FROM awardssharemanagers;
-SELECT * FROM awardsshareplayers
-SELECT * FROM batting;
-SELECT * FROM battingpost;
-SELECT * FROM collegeplaying;
-SELECT * FROM fielding;
-SELECT * FROM fieldingof;
-SELECT * FROM fieldingofsplit;
-SELECT * FROM fieldingpost;
-SELECT * FROM halloffame;
-SELECT * FROM homegames;
-SELECT * FROM managers;
-SELECT * FROM managershalf;
-SELECT * FROM parks;
-SELECT * FROM people;
-SELECT * FROM pitching;
-SELECT * FROM pitchingpost;
-SELECT * FROM salaries;
-SELECT * FROM schools;
-SELECT * FROM seriespost;
-SELECT * FROM teams;
-SELECT * FROM teamsfranchises;
-SELECT * FROM teamshalf;
-
-
 --Question 1
 SELECT min(yearid) AS earliest_year, max(yearid) AS latest_year
 FROM teams;
@@ -237,6 +208,34 @@ HAVING SUM(hr) > 0
 AND most_hrs = hr
 ORDER BY max_hr DESC
 */
+
+SELECT * FROM allstarfull;
+SELECT * FROM appearances;
+SELECT * FROM awardsmanagers;
+SELECT * FROM awardsplayers;
+SELECT * FROM awardssharemanagers;
+SELECT * FROM awardsshareplayers
+SELECT * FROM batting;
+SELECT * FROM battingpost;
+SELECT * FROM collegeplaying;
+SELECT * FROM fielding;
+SELECT * FROM fieldingof;
+SELECT * FROM fieldingofsplit;
+SELECT * FROM fieldingpost;
+SELECT * FROM halloffame;
+SELECT * FROM homegames;
+SELECT * FROM managers;
+SELECT * FROM managershalf;
+SELECT * FROM parks;
+SELECT * FROM people;
+SELECT * FROM pitching;
+SELECT * FROM pitchingpost;
+SELECT * FROM salaries;
+SELECT * FROM schools;
+SELECT * FROM seriespost;
+SELECT * FROM teams;
+SELECT * FROM teamsfranchises;
+SELECT * FROM teamshalf;
 
 
 
