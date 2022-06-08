b65531a8d57d35dd76a22cd663c6c3c1cfc88d8e
michaelcnorman <mcnorman86@gmail.com>
2022-06-08 15:33:46 -0500

    michaels code including q4-q8-q10+notes on other questions

--- data/michael.sql
+++ data/michael.sql
@@ -28,6 +28,19 @@ WHERE sch.schoolid = 'vandy'
 GROUP BY p.namefirst, p.namelast
 ORDER BY total_salary DESC;
 
+--michael attempt on q3
+WITH vandy AS (SELECT DISTINCT cp.playerid, s.schoolname
+				FROM collegeplaying AS cp INNER JOIN schools AS s USING(schoolid)
+				WHERE s.schoolname ILIKE '%vanderbilt%'),
+	 s AS (SELECT playerid, salary::numeric::money AS salary
+			   	FROM salaries)
+SELECT CONCAT(p.namefirst, ' ',p.namelast), schools.schoolname, salary
+FROM people AS p INNER JOIN vandy USING (playerid)
+			INNER JOIN s USING (playerid)
+			INNER JOIN collegeplaying AS cp USING (playerid)
+			INNER JOIN schools USING (schoolid)
+ORDER BY salary DESC; --attempt on q3
+
 /*Q4. Using the fielding table, group players into three groups based on their position: 
 label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", 
 and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three 
@@ -87,8 +100,17 @@ defined as total attendance divided by number of games). Only consider parks whe
 there were at least 10 games played. Report the park name, team name, and average attendance. 
 Repeat for the lowest 5 average attendance.*/
 
+-- Top 5
+SELECT homegames.team, parks.park_name,homegames.park AS park_id, (homegames.attendance / homegames.games) AS avg_attendance
+FROM homegames INNER JOIN parks USING(park)
+WHERE year = 2016
+GROUP BY homegames.team, homegames.park, parks.park_name,homegames.attendance, homegames.games
+HAVING games > 9
+ORDER BY avg_attendance DESC
+LIMIT 5;
 
-SELECT homegames.team, parks.park_name AS park_id,homegames.park, (homegames.attendance / homegames.games) AS avg_attendance
+-- Bottom 5
+SELECT homegames.team, parks.park_name,homegames.park AS park_id, (homegames.attendance / homegames.games) AS avg_attendance
 FROM homegames INNER JOIN parks USING(park)
 WHERE year = 2016
 GROUP BY homegames.team, homegames.park, parks.park_name,homegames.attendance, homegames.games
@@ -102,32 +124,31 @@ and who hit at least one home run in 2016. Report the players' first and last na
 and the number of home runs they hit in 2016.*/
 -- Use a window function to compare HR to MaxHR for each player and place in CTE
 
-SELECT DISTINCT playerid, yearid, MAX(hr)
+
+WITH a AS (SELECT b.playerid,
+		  ((p.finalgame::date)-(p.debut::date))/365 AS years_played
+		  FROM people AS p INNER JOIN batting AS b USING(playerid)),
+	 b AS (SELECT DISTINCT playerid, MAX(hr) AS most_hr
+			FROM batting
+			GROUP BY playerid)
+SELECT DISTINCT CONCAT(p.namefirst,' ',p.namelast) AS full_name, hr AS home_runs
+FROM a INNER JOIN b USING(playerid)
+	   INNER JOIN people AS p USING (playerid)
+	   INNER JOIN batting USING (playerid)
+WHERE a.years_played > 9
+	AND hr > 0
+	AND yearid = 2016
+	AND most_hr = hr
+ORDER BY home_runs DESC;
+
+SELECT DISTINCT playerid, MAX(hr) AS most_hr
 FROM batting
-GROUP BY playerid, yearid
-ORDER BY MAX(hr) DESC;
-
-
-SELECT p.namefirst, p.namelast, b.yearid, ((p.finalgame::date)-(p.debut::date))/365 AS years_played
-FROM people AS p INNER JOIN batting AS b USING(playerid); --calculated years played
-
-WITH a AS (SELECT b.yearid, 
-					  ((p.finalgame::date)-(p.debut::date))/365 AS years_played
-					  FROM people AS p INNER JOIN batting AS b USING(playerid))
-SELECT * 
-FROM a 
-WHERE years_played > 9
+GROUP BY playerid
 
 
 
-SELECT p.namefirst, p.namelast, MAX(hr) AS max_hr
-FROM batting AS b INNER JOIN people AS p USING(playerid)
-WHERE yearid = 2016
-GROUP BY p.namefirst, p.namelast;
-
-
-SELECT CONCAT(namefirst,' ',namelast)
-FROM people;--CONCAT example
+/*SELECT CONCAT(namefirst,' ',namelast)
+FROM people;--CONCAT example*/
 
 
 
